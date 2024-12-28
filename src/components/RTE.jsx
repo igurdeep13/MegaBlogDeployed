b0d8c2ba93491b906ab79a016aa82163c5f47cc1
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-12-27 22:07:51 -0500

    add api key in tinyMce

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
-
+import conf from "../conf/conf";
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
@@ -12,7 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="xd71axix4ola1pihm9e2ukkj7zmagi5qww64rmwgflaketdb"
+            apiKey={conf.tinyMceApIKey}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
